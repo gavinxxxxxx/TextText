3fdcb389435956a23f3ebe2d1b98322f74b68b2d
gavin <gavinxxxxxx@gmail.com>
2018-01-20 22:46:17 +0800

    FastScroll

---
+++ app/src/main/java/me/gavin/widget/FastScrollLayout.java
@@ -0,0 +1,22 @@
+package me.gavin.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.ViewGroup;
+
+/**
+ * 这里是萌萌哒注释菌
+ *
+ * @author gavin.xiong 2018/1/20.
+ */
+public class FastScrollLayout extends ViewGroup {
+
+    public FastScrollLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+
+    }
+}
